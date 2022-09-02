9a579414eef856fd26b3ffbb46031040aad97070
Alexey Akulich <loki1987by@yandex.ru>
2022-09-02 15:52:10 +0300

    fix&docs: fix api & upd readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # currency-converter-angular
+#### Single Page Application "Конвертер валют" написанный на angular JS.
+#### Деплой проекта (здесь)[currency-converter-angular.netlify.app]
--- src/app/utils/Api.js
+++ src/app/utils/Api.js
@@ -1,18 +1,32 @@
-const apiKey = "rzs8g6nzYrcupbHN1k8vjBYDxlzzUxOy";
+const apiKey = "8Iv0t8apaL2xQPIA3lISZHbz3H6VsN90";
+const sorry = `Сайт работает в ознакомительном режиме.
+  В связи с этим есть ограничения по количеству запросов к информационным службам.
+  Если вы вы видите это сообщение значит превышено дневное/месячное ограничение запросов.
+  Будьте добры сообщить о неполадках разработчику.
+  Приносим извинения за временные неудобства.
+  Можете попробовать запустить страницу спустя сутки.
+  Спасибо за внимание.`;
 
 export function getAllCurrencies() {
   return fetch("https://api.apilayer.com/currency_data/list", {
     method: "GET",
     headers: {
-      apikey: apiKey,
-    },
+      apikey: apiKey
+    }
   })
-    .then((response) => response.json())
-    .catch((error) => console.log("error", error));
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert(sorry);
+      }
+    })
+    .catch(error => console.log("error", error));
 }
 
 export function getRates(currency) {
   let value = "USD";
+
   if (currency) {
     value = currency;
   }
@@ -21,18 +35,30 @@ export function getRates(currency) {
     {
       method: "GET",
       headers: {
-        apikey: apiKey,
-      },
+        apikey: apiKey
+      }
     }
   )
-    .then((response) => response.json())
-    .catch((error) => console.log("error", error));
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert(sorry);
+      }
+    })
+    .catch(error => console.log("error", error));
 }
 
 export function getLocalCurrency() {
   return fetch("http://ip-api.com/json?fields=currency", {
-    method: "GET",
+    method: "GET"
   })
-    .then((response) => response.json())
-    .catch((error) => console.log("error", error));
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert(sorry);
+      }
+    })
+    .catch(error => console.log("error", error));
 }
