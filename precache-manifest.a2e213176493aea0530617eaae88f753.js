ff77684d2fe55cd98240d570162d8074a8df1449
Alexey Akulich <loki1987by@yandex.ru>
2022-09-02 20:21:56 +0300

    Updates

---
+++ precache-manifest.a2e213176493aea0530617eaae88f753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c6b4cafc71cb6155b5ce5a4225670f",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "cb694685b5721851bf1e",
+    "url": "/currency-converter/static/css/main.adb754c2.chunk.css"
+  },
+  {
+    "revision": "895f61d483533556e783",
+    "url": "/currency-converter/static/js/2.ceda28a2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/currency-converter/static/js/2.ceda28a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb694685b5721851bf1e",
+    "url": "/currency-converter/static/js/main.bd4a1f2a.chunk.js"
+  },
+  {
+    "revision": "3198c54874c932ccedad",
+    "url": "/currency-converter/static/js/runtime-main.29930c2d.js"
+  }
+]);
